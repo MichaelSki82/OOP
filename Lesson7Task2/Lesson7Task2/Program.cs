408f2f34dae1b406b4b1d24a56f2889156499336
MichaelSki82 <romantik1001@yandex.ru>
2021-08-28 11:02:43 +0300

    Структуры данных

---
+++ Lesson7Task2/Lesson7Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson7Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "The best from the best";
+            char c = 't';
+            int i = s.CountElement(c);
+
+            Console.WriteLine(i);
+
+            Console.Read();
+        }
+    }
+
+    public static class String
+    {
+        public static int CountElement(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
---
+++ Lesson7Task3/Lesson7Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Lesson7Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //дана произвольная коллекция List<T>.Сколько раз встречается каждый элемнт в данной коллекции
+
+            List<int> intList = new List<int>() { 6, 5, 4, 6, 3, 2, 12, 100, 100, 52, 3, 6, 99, 12 };
+
+            Dictionary<int, int> intSpesial = new Dictionary<int, int>();
+            
+            foreach (int i in intList)
+            {
+                if (intSpesial.ContainsKey(i))
+                {
+                    intSpesial[i]++;
+                }
+                else
+                {
+                    intSpesial[i] = 1;
+                }
+            }
+
+            foreach(KeyValuePair<int, int> x in intSpesial)
+            {
+                Console.WriteLine($"Элемент {x.Key}  встречается {x.Value} раз");
+            }
+
+            
+
+        }
+    }
+}
